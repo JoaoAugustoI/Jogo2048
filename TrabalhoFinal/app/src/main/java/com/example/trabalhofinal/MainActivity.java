6f93a97e5d4c5370bd53858425baef489b3ced8d
Lara Ferigatto <ferigatto1994@gmail.com>
2020-06-11 18:36:33 -0300

    pagina inicial

--- TrabalhoFinal/app/src/main/java/com/example/trabalhofinal/MainActivity.java
+++ TrabalhoFinal/app/src/main/java/com/example/trabalhofinal/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.trabalhofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int teste = 0;
+        play = findViewById(R.id.play);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, game.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
---
+++ TrabalhoFinal/app/src/main/java/com/example/trabalhofinal/game.java
@@ -0,0 +1,15 @@
+package com.example.trabalhofinal;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class game extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_game);
+    }
+}
