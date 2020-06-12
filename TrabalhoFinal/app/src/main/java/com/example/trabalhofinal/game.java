48be2fff010720ce6c07faba267d311167ea2ba5
IonaSantana <ionasantana123@gmail.com>
2020-06-12 19:05:21 -0300

    os trem de pause e replay

--- TrabalhoFinal/app/src/main/java/com/example/trabalhofinal/game.java
+++ TrabalhoFinal/app/src/main/java/com/example/trabalhofinal/game.java
@@ -1,6 +1,10 @@
 package com.example.trabalhofinal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +15,29 @@ public class game extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_game);
+
+        final ImageView replay = (ImageView) findViewById(R.id.replay);
+        final ImageView pause = (ImageView) findViewById(R.id.pause);
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i =  new Intent(game.this,Pause.class);
+                startActivity(i);
+            }
+        });
+
+        replay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i =  new Intent(game.this, game.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        
     }
 }
